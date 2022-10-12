21d7894802be2092ed739b03628e2ee993a064aa
camisbrussi <camila.sbrussi@universo.univates.br>
2022-10-12 12:22:23 -0300

    step 2 - dinamic data, connect api

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
 import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/Header";
 import { Card } from "./components/Card";
+import axios from "axios";
 
 import styles from "./App.module.css";
 
 import "./global.css";
+import { useEffect, useState } from "react";
+
+export interface Pix {
+  id: number;
+  value: number;
+  createdAt: string;
+  recipient: {
+    name: string;
+  };
+  sender: {
+    name: string;
+  };
+  type?: number;
+}
 
 function App() {
+  const [pixList, setPixList] = useState<Pix[]>([]);
+
+  useEffect(() => {
+    axios.get<Pix[]>("http://localhost:3333/pix").then((response) => {
+      setPixList(response.data);
+    });
+  }, []);
+
+  console.log(pixList);
   return (
     <div className="App">
       <Header showButton />
       <Sidebar />
       <main className={styles.content}>
-        <Card newPix />
-        <Card newPix send />
-        <Card />
-        <Card send />
-        <Card />
+        {pixList.map((pix) => {
+          return (
+            <Card
+              id={pix.id}
+              value={pix.value}
+              createdAt={pix.createdAt}
+              recipient={pix.recipient}
+              sender={pix.sender}
+              type={pix.type}
+            />
+          );
+        })}
       </main>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,45 @@
+import { Pix } from "../App";
 import styles from "./Card.module.css";
 import { CardInfo } from "./CardInfo";
+import { format, formatDistance } from "date-fns";
+import ptBr from "date-fns/locale/pt-BR";
 
-export function Card({ newPix = false, send = false }: any) {
+export function Card({
+  id,
+  value,
+  createdAt,
+  recipient,
+  sender,
+  type = 1,
+}: Pix) {
+  const formattedDate = format(new Date(createdAt), "dd MMM", {
+    locale: ptBr,
+  });
+
+  const formattedValue = value.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  const isNewPix = formatDistance(new Date(createdAt), new Date());
+
   return (
-    <div className={`${styles.card} ${newPix && styles.new}`}>
-      {newPix && (
+    <div className={`${styles.card} ${isNewPix && styles.new}`}>
+      {isNewPix && (
         <div className={styles.newPix}>
           <span>Novo</span>
         </div>
       )}
       <div className={styles.content}>
-        <strong>1</strong>
+        <strong>{id}</strong>
 
-        <CardInfo title="REMETENTE" data="Ronald Richards" />
-        <CardInfo title="Destinário" data="Ronald Richards" />
-        <CardInfo title="Data" data="Hoje" />
-        <CardInfo title="Valor" data="10,00" />
+        <CardInfo title="REMETENTE" data={sender.name} />
+        <CardInfo title="Destinário" data={recipient.name} />
+        <CardInfo title="Data" data={formattedDate} />
+        <CardInfo title="Valor" data={formattedValue} />
 
-        <strong className={send ? styles.send : styles.received}>
-          {send ? "EMVIADO" : "RECEBIDO"}
+        <strong className={type === 1 ? styles.send : styles.received}>
+          {type === 1 ? "ENVIADO" : "RECEBIDO"}
         </strong>
       </div>
     </div>
